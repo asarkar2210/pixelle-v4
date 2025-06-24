62576ad90758a126e2882ee3f87e44f5779add1d
Ayushman Sarkar <ayushhmansarkar@gmail.com>
2025-06-25 01:32:40 +0530

    Added 'upload image' ability to 'configure/upload'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,8 +34,13 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavBar />
-        {children}
-        <Footer />
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            {children}
+          </div>
+          <Footer />
+        </main> 
+        <Toaster position="bottom-center" richColors/>
       </body>
     </html>
   );
